9b56a09443a5567488b8d812615c30d3eeca597d
Glendis <auraregrm@gmail.com>
2020-09-25 23:03:05 -0500

    se añaden alertas

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,10 +1,11 @@
+const ordenLetras = 'ABCDEFGHIJ';
+
 
 function theme(tema){
     limpiarTema();
     let cuerpo = document.getElementById('cuerpo');
     cuerpo.classList.add(tema);
     localStorage.setItem("tema", tema);
-    console.log('el tema es' + localStorage.getItem("tema"));
 }
 
 function temaGuardado(){
@@ -17,18 +18,48 @@ function temaGuardado(){
     }
 
 }
+
 function limpiarTema(){
     let cuerpo = document.getElementById('cuerpo');
     cuerpo.classList.remove('retro');
     cuerpo.classList.remove('dark');
     
 }
+
 function presionar(letra, numero){
 
     document.getElementById('letras').value=document.getElementById('letras').value+letra;
     document.getElementById('numeros').value=document.getElementById('numeros').value+numero;
 
+    let letras  = document.getElementById('letras').value;
+    let numeros = document.getElementById('numeros').value;
+
+    let bandera = false;
+
+    for (let indice = 0; indice < letras.length; indice ++)
+    {
+        if(ordenLetras.charAt(indice) !== letras.charAt(indice))
+        {
+            swal({
+                title: "Empieza otra vez!",
+                text: "El orden no es correcto!",
+                icon: "error",
+              });
+              bandera = true;
+              defecto();
+        }
+    }
+        if (ordenLetras.length === letras.length && bandera !==true){
+            swal({
+                title: "Has cumplido con el reto",
+                text: "El orden es correcto!",
+                icon: "success",
+              });
+              defecto();
+        }
 }
+
+
 
 function defecto(){
     limpiarTema();
